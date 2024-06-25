c4277eace4634da2efb5085fb639b50fe8985227
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-06-25 14:08:43 +0200

    Component folders have been created

---
+++ src/Hooks/__hooks_instruction.js
@@ -0,0 +1,4 @@
+
+
+// all hooks will be here 
+// ex.: AllUsers.jsx, AllItems.jsx, AxiosPublic, and so on 
---
+++ src/SharedComponets/__instruction.js
@@ -0,0 +1,3 @@
+// all components of the landing page will be in this folder.
+// seperate folder is not prblem at all
+  

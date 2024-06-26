ce45ee24e9f9ae40717e61be3a150e324d5c7fa6
sadekcric4 <sadekcric4@gmail.com>
2024-06-26 17:11:41 +0600

    sadekur Rahman Push commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello Group</h1>
+      <h1>I am Sadekur Rahman....</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

f99981b6ff59f4011fc15a9c1830727db7e206e3
saifullah-10 <saifullah.bus@gmail.com>
2024-06-26 22:45:14 +0600

    update from rafi

--- src/SharedComponets/Banner/Banner.jsx
+++ src/SharedComponets/Banner/Banner.jsx
@@ -1,15 +1,14 @@
-
-
 const Banner = () => {
-    return (
-        <div>
-            <h1>This is Banner</h1>
-            <h2>Change for Nahar </h2>
-            <h3>Test for ZK </h3>
-            <h3>Bangladesh</h3>
-            <p>Paragraph from farha</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is Banner</h1>
+      <h2>Change for Nahar </h2>
+      <h3>Test for ZK </h3>
+      <h3>Bangladesh</h3>
+      <p>Paragraph from farha</p>
+      <h1>from rafi </h1>
+    </div>
+  );
 };
 
 export default Banner;

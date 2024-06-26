3ce6cf8aba17252bec302c61b9ec4782017917f8
saifullah-10 <saifullah.bus@gmail.com>
2024-06-26 23:22:00 +0600

    update from rafi

--- src/SharedComponets/Footer/Footer.jsx
+++ src/SharedComponets/Footer/Footer.jsx
@@ -1,11 +1,9 @@
-
-
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <p>welcome to footer</p>
+    </div>
+  );
 };
 
 export default Footer;

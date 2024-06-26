438eb278e49c3c792bad209d1e015238cbb72ec0
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-06-26 22:22:18 +0600

    banner paragraph added

--- src/SharedComponets/Banner/Banner.jsx
+++ src/SharedComponets/Banner/Banner.jsx
@@ -7,6 +7,7 @@ const Banner = () => {
             <h2>Change for Nahar </h2>
             <h3>Test for ZK </h3>
             <h3>Bangladesh</h3>
+            <p>Paragraph from farha</p>
         </div>
     );
 };

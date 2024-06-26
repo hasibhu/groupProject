10a22ecabcf3eab12b40b5ef9d82f1796f0705cd
Zioun <jionkhan0@gmail.com>
2024-06-26 22:56:06 +0600

    added

--- src/SharedComponets/Banner/Banner.jsx
+++ src/SharedComponets/Banner/Banner.jsx
@@ -7,10 +7,11 @@ const Banner = () => {
             <h2>Change for Nahar </h2>
             <h3>Test for ZK </h3>
             <h3>Bangladesh</h3>
-
             <h4>Pakistan</h4>
             <h5>Afgan</h5>
             <h6>Iran</h6>
+
+            <h1>Saudi</h1>
         </div>
     );
 };

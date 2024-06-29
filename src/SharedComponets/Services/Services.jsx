72c565943fced39806971bec78dc7a939104281e
saifullah-10 <saifullah.bus@gmail.com>
2024-06-29 17:47:50 +0600

    update custom components

--- src/SharedComponets/Services/Services.jsx
+++ src/SharedComponets/Services/Services.jsx
@@ -3,7 +3,7 @@ import { FaServer, FaUserLock } from "react-icons/fa";
 import { MdAdminPanelSettings, MdDashboard, MdDevices } from "react-icons/md";
 import "./services.css";
 import { IoIosSettings, IoLogoUsd } from "react-icons/io";
-import SectionTitle from "../../CustomeComponent/SectionTitle";
+import SectionTitle from "../../CustomComponent/SectionTitle";
 
 const Services = () => {
   const title = `Services`;
@@ -12,18 +12,6 @@ const Services = () => {
           Harry Up .....`;
   return (
     <div>
-      {/* <div className=" max-w-[1440px] mx-auto flex flex-col items-center justify-center">
-        <h1 className="bg-[#FDEFF2] inline-block uppercase text-[#E43C5C] text-[16px] font-semibold py-1 px-5 rounded-full">
-          Services
-        </h1>
-        <h2 className="text-[#444444] text-[40px] font-semibold">
-          Our <span className="text-[#E43C5C]">Services</span>
-        </h2>
-        <p className=" text-lg text-center">
-          If you buy any services from us we commited to support you 24/7. So
-          Harry Up .....
-        </p>
-      </div> */}
       <div>
         <SectionTitle
           title={title}

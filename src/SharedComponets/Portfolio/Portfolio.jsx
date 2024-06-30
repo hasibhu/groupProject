1f18e1d009afc044e5ed5503aff63bf01c4d93c0
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-07-01 00:02:43 +0600

    done

--- src/SharedComponets/Portfolio/Portfolio.jsx
+++ src/SharedComponets/Portfolio/Portfolio.jsx
@@ -7,7 +7,7 @@ const Portfolio = () => {
   const description =
     "Step into our project gallery where each piece of work reflects our dedication and skill in coding.  From sleek websites to complex applications, CodingLab’s projects demonstrate our ability to tackle challenges and deliver exceptional results.";
   return (
-    <div className="w-[1440px] mx-auto my-10">
+    <div className="max-w-[1440px] mx-auto my-10">
       <div className="mb-4">
         <SectionTitle
           title={title}
